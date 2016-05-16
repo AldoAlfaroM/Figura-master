663e413dcde39af23a53bbc316ab83b0919276b6
aldomijastec <aldoaso10@gmail.com>
2016-05-16 10:34:36 -0700

    cambios actualizados
    linea, botones, color

---
+++ WindowsFormsApplication2/Figura.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    abstract class Figura:IComparable
+    {
+        protected int X;
+        protected int Y;
+        protected Pen pluma,pluma1;
+        protected int ancho;
+        protected int largo;
+        protected Color color;
+        protected SolidBrush brocha;
+
+        public Figura(int x, int y )
+        {
+            X = x;
+            Y = y;
+            brocha = new SolidBrush(Color.DarkBlue);
+            pluma = new Pen(Color.White, 2);
+            pluma1 = new Pen(Color.FromArgb(255,0,0,0));
+            Random rnd = new Random();
+            ancho = rnd.Next(10,70);
+            largo = ancho;
+        }
+
+        public abstract void Draw(Form f);
+
+        public int CompareTo(object obj)
+        {
+            return this.largo.CompareTo(((Figura)obj).largo);
+        }
+    }
+
+
+    class Rectangulo:Figura
+    {
+        public Rectangulo(int x, int y ):base(x,y)
+    	{
+        } 
+
+        public override void Draw(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawRectangle(pluma, this.X, this.Y, ancho, largo);
+            g.FillRectangle(brocha, this.X, this.Y, ancho, largo);
+        }
+    }
+
+    class Circulo : Figura
+    {
+        public Circulo(int x, int y) : base(x, y)
+        {
+        }
+
+        public override void Draw(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawEllipse(pluma, this.X, this.Y, ancho, largo);
+            g.FillEllipse(brocha, this.X, this.Y, ancho, largo);
+        }
+    }
+    class Linea : Figura
+    {
+        public Linea(int x, int y) : base(x, y)
+        {
+        }
+
+        public override void Draw(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawLine(pluma1, X, Y, X + largo, Y+ ancho);
+        }
+    }
+}
---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.figuraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectanguloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.circuloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ordenarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lineaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem,
+            this.figuraToolStripMenuItem,
+            this.ordenarToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 92);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // figuraToolStripMenuItem
+            // 
+            this.figuraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rectanguloToolStripMenuItem,
+            this.circuloToolStripMenuItem,
+            this.lineaToolStripMenuItem});
+            this.figuraToolStripMenuItem.Name = "figuraToolStripMenuItem";
+            this.figuraToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.figuraToolStripMenuItem.Text = "Figura";
+            // 
+            // rectanguloToolStripMenuItem
+            // 
+            this.rectanguloToolStripMenuItem.Name = "rectanguloToolStripMenuItem";
+            this.rectanguloToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.rectanguloToolStripMenuItem.Text = "Rectangulo";
+            this.rectanguloToolStripMenuItem.Click += new System.EventHandler(this.rectanguloToolStripMenuItem_Click);
+            // 
+            // circuloToolStripMenuItem
+            // 
+            this.circuloToolStripMenuItem.Name = "circuloToolStripMenuItem";
+            this.circuloToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.circuloToolStripMenuItem.Text = "Circulo";
+            this.circuloToolStripMenuItem.Click += new System.EventHandler(this.circuloToolStripMenuItem_Click);
+            // 
+            // ordenarToolStripMenuItem
+            // 
+            this.ordenarToolStripMenuItem.Name = "ordenarToolStripMenuItem";
+            this.ordenarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ordenarToolStripMenuItem.Text = "Ordenar";
+            this.ordenarToolStripMenuItem.Click += new System.EventHandler(this.ordenarToolStripMenuItem_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.Location = new System.Drawing.Point(406, 247);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(91, 27);
+            this.bt2.TabIndex = 2;
+            this.bt2.Text = "Limpiar";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(406, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 27);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lineaToolStripMenuItem
+            // 
+            this.lineaToolStripMenuItem.Name = "lineaToolStripMenuItem";
+            this.lineaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.lineaToolStripMenuItem.Text = "Linea";
+            this.lineaToolStripMenuItem.Click += new System.EventHandler(this.lineaToolStripMenuItem_Click);
+            // 
+            // frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(538, 317);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.bt2);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "frm";
+            this.Text = "Dibuja Figuras";
+            this.BackColorChanged += new System.EventHandler(this.button2_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem figuraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectanguloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem circuloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ordenarToolStripMenuItem;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem lineaToolStripMenuItem;
+    }
+}
+
